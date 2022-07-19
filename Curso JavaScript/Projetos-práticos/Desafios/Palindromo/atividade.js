b9b1621197dacc93a9b73430c94bf7432805ec8a
Marcos <contatomvcoutinho@outlook.com>
2022-07-19 20:17:29 -0300

    Resolução de desafios

---
+++ Curso JavaScript/Projetos-práticos/Desafios/Palindromo/atividade.js
@@ -0,0 +1,21 @@
+// Solução 1.
+function verificaPalindromo(string){
+    if (!string) return "string inexistente";
+
+    return string.split(" ").reverse().join(" ") === string;
+}
+
+// Solução 2.
+
+function verificaPalindromo2(string){
+    if (!string) return "string inexistente";
+
+    for(let i = 0; i < string.lenght / 2; i++){
+        if (string[i] !== string[string.lenght -1 -i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(verificaPalindromo2("abba"));
